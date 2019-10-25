db763f26c0d0daa5d0a71c67015fd4545551d92f
Tomas Bellus <tomas.bellus@gmail.com>
2019-10-25 16:29:39 +0200

    fix frontend role handlers and add dev playbook

---
+++ roles/brewmaster-frontend/files/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:80',
+        'webpack/hot/only-dev-server',
+         path.join(__dirname, '/src/index.tsx')
+    ],
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
